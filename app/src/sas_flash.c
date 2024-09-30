e5505c874870d30c7051ea6de1e492fb5be38b38
Saurabh Kumar <Saurabh.Kumar@polaris.com>
2024-09-30 15:54:00 +0530

    Error ok added

--- app/src/sas_flash.c
+++ app/src/sas_flash.c
@@ -2,55 +2,55 @@
 
 /* Erase Functions */
 sas_retype erase_application() {
-
+ return SAS_ERR;
 }
 
 sas_retype erase_page() {
-
+return SAS_ERR;
 }
 
 sas_retype erase_address() {
-
+return SAS_ERR;
 }
 
 
 /* Write/Program Functions */
 sas_retype write_word() {
-
+return SAS_ERR;
 }
 
 sas_retype write_byte() {
-
+return SAS_ERR;
 }
 
 sas_retype write_address() {
-
+return SAS_ERR;
 }
 
 sas_retype program_page() {
-
+return SAS_ERR;
 }
 
 sas_retype program_application() {
-
+return SAS_ERR;
 }
 
 
 /* Read Flash Memory Functions */
 sas_retype read_application() {
-
+return SAS_ERR;
 }
 
 sas_retype read_address() {
-
+return SAS_ERR;
 }
 
 
 /* Flash Functions */
 sas_retype unlock_flash() {
-
+return SAS_ERR;
 }
 
 sas_retype lock_flash() {
-
+return SAS_ERR;
 }
